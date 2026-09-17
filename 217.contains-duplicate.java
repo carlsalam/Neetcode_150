20ffe1be2f3c5ec3609f8adf1ff3504facf14329
carlsalam <carl.salamouny.1@ens.etsmtl.ca>
2026-09-17 16:40:35 -0400

    solved 217 contains dups

--- 217.contains-duplicate.java
+++ 217.contains-duplicate.java
@@ -5,9 +5,34 @@
  */
 
 // @lc code=start
+
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
+        HashSet<Integer> seen = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(seen.contains(nums[i])){
+                return true;
+            }
+            seen.add(nums[i]);
+        }   
+
+        return false;
+
+
+        // int n = nums.length;
+        // for(int i = 0;i< n; i++ ){
+        //     for(int j = i+1; j< n; j++)
+        //     {
+        //         if(nums[i] == nums[j])
+        //             {
+        //                 return true;
+        //             }
+        //     }
+        // }
+        // return false;
     }
 }
 // @lc code=end
