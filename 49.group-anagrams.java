9623f36c55ca9c3f4109fc11d4c54b9db43a509a
carlsalam <carl.salamouny.1@ens.etsmtl.ca>
2026-09-19 21:54:13 -0400

    solved 49 group anagrams

--- 49.group-anagrams.java
+++ 49.group-anagrams.java
@@ -5,9 +5,35 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+        HashMap<String, List<String>> groups = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            // logique pr sort chq mots du array de string
+            char[] letters = strs[i].toCharArray();
+            Arrays.sort(letters);
+            String word = new String(letters);
+
+            if(!groups.containsKey(word)){
+                groups.put(word, new ArrayList<String>());
+            }
+                groups.get(word).add(strs[i]);
+
+        }
+
+        System.out.println(groups);
+
+        List<List<String>> answer = new ArrayList<>(groups.values());
+
+    
+        return answer;
     }
 }
 // @lc code=end
